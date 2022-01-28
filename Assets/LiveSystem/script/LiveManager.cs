385a5258e242bc495c5932d4d5275f62707da77f
jiho1208 <jihojiho1208@naver.com>
2022-01-28 16:30:38 +0900

    판정 구현

--- Assets/LiveSystem/script/LiveManager.cs
+++ Assets/LiveSystem/script/LiveManager.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class LiveManager : MonoBehaviour
 {
     public List<GameObject> Decision;
-    private float Wait = 0.1f;
+    public List<Transform> SpawnPoint;
+    public GameObject Note;
+    public float SpawnDelay;
 
     void Start()
     {
-        
+        StartCoroutine(NoteSpawn());
     }
 
     void Update()
@@ -22,32 +24,53 @@ public class LiveManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A))
         {
             Decision[0].SetActive(true);
-            StartCoroutine(DecisionOff(Decision[0]));
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            Decision[0].SetActive(false);
         }
 
         if (Input.GetKeyDown(KeyCode.S))
         {
             Decision[1].SetActive(true);
-            StartCoroutine(DecisionOff(Decision[1]));
+        }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            Decision[1].SetActive(false);
         }
 
         if (Input.GetKeyDown(KeyCode.Semicolon))
         {
             Decision[2].SetActive(true);
-            StartCoroutine(DecisionOff(Decision[2]));
+        }
+        
+        if (Input.GetKeyUp(KeyCode.Semicolon))
+        {
+            Decision[2].SetActive(false);
         }
 
         if (Input.GetKeyDown(KeyCode.Quote))
         {
             Decision[3].SetActive(true);
-            StartCoroutine(DecisionOff(Decision[3]));
+        }
+
+        if (Input.GetKeyUp(KeyCode.Quote))
+        {
+            Decision[3].SetActive(false);
         }
 
     }
 
-    IEnumerator DecisionOff(GameObject OffObject)
+    IEnumerator NoteSpawn()
     {
-        yield return new WaitForSeconds(Wait);
-        OffObject.SetActive(false);
+        WaitForSeconds Wait = new WaitForSeconds(SpawnDelay);
+        while (true)
+        {
+        int RandomSpawn = Random.Range(0, 4);
+        Instantiate(Note, SpawnPoint[RandomSpawn]);
+        yield return Wait;
+        }
     }
 }
--- Assets/LiveSystem/script/Note.cs
+++ Assets/LiveSystem/script/Note.cs
@@ -5,22 +5,58 @@ using UnityEngine;
 public class Note : MonoBehaviour
 {
     public int NoteSpeed;
+    public float Perfect = 0.49f;
+    public float Great = 0.9f;
+    public float Good = 1.19f;
+    public float Bad = 1.5f;
 
     void Start()
     {
-       
+
     }
 
     void Update()
     {
-        transform.Translate(Vector3.down * Time.deltaTime * NoteSpeed);
+        Move();
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("EndLine"))
+        if (other.gameObject.CompareTag("Decision"))
         {
+            if (Mathf.Abs(transform.position.z) <= Perfect)
+            {
+                Debug.Log("Perfect");
+            }
+            else if (Mathf.Abs(transform.position.z) <= Great)
+            {
+                Debug.Log("Great");
+            }
+            else if (Mathf.Abs(transform.position.z) <= Good)
+            {
+                Debug.Log("Good");
+            }
+            else if (Mathf.Abs(transform.position.z) <= Bad)
+            {
+                Debug.Log("Bad");
+            }
+            else
+            {
+                Debug.Log("Miss");
+            }
             Destroy(this.gameObject);
         }
+
+        if (other.gameObject.CompareTag("EndLine"))
+        {
+            Debug.Log("miss");
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    void Move()
+    {
+        transform.Translate(Vector3.down * Time.deltaTime * NoteSpeed);
     }
 }
